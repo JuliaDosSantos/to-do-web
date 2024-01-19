26d94741ecba90a7be86c84c96fbdeabc7882bc4
JuliaDosSantos <santos.ju1995@gmail.com>
2024-01-18 23:27:38 -0300

    rodapé

---
+++ src/views/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import * as S from './styles';
+
+//NOSSOS COMPONENTES
+import Header from '../../components/Header';
+import Footer from  '../../components/Footer';
+
+function Home() {
+  return (
+    <S.Container>
+     <Header/>
+     <Footer/>
+    </S.Container>
+  )
+}
+
+export default Home;
