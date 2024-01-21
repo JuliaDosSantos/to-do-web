5a5ea0e2a18d001e74331f4dfc8a75f289c3da9c
JuliaDosSantos <santos.ju1995@gmail.com>
2024-01-21 18:53:02 -0300

    filter cards

--- src/views/Home/styles.js
+++ src/views/Home/styles.js
@@ -4,3 +4,14 @@ export const Container = styled.div`
     width: 100%;
    
 `
+export const FilterArea = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    margin-top: 30px;
+
+    button{
+        background: none;
+        border: none;
+    }
+`
