c3d5aad9d0c72dd3814637a25b4c70c4ce18b08e
JuliaDosSantos <santos.ju1995@gmail.com>
2024-02-13 22:38:39 -0300

    tela de tarefas

--- src/views/Task/index.js
+++ src/views/Task/index.js
@@ -8,8 +8,12 @@ import Header from '../../components/Header';
 import Footer from  '../../components/Footer';
 import TypeIcons from '../../utils/typeIcons';
 
+import iconCalendar from '../../assets/calendar.png';
+import iconClock from '../../assets/clock.png';
+
 function Task() {
     const [lateCount, setLateCount] = useState();
+    const [type, setType] = useState();
  
     async function lateVerify(){
       await api.get(`/task/filter/late/11:11:111:111:111:11`)
@@ -31,13 +35,38 @@ function Task() {
         <S.TypeIcons>
           {
             TypeIcons.map((icon, index) => (
-              index > 0 && <img src={icon} alt = "Tipo da tarefa"/>
-
+              index > 0 && 
+              <button type="button" onClick = {() => setType(index)}>
+                <img src={icon} alt = "Tipo da tarefa"
+                className = {type && type != index && 'inative'}/>
+              </button>
             ))
           }
 
         </S.TypeIcons>
 
+        <S.Input>
+          <span>Título</span>
+          <input type="text" placeholder = "Título da tarefa..."></input>
+        </S.Input>
+
+        <S.TextArea>
+          <span>Título</span>
+          <textarea rows = {5} placeholder = "Detalhes da tarefa..." />
+        </S.TextArea>
+
+        <S.Input>
+          <span>Data</span>
+          <input type="date" placeholder = "Título da tarefa..."></input>
+          <img src = {iconCalendar} alt = "Calendário"/>
+        </S.Input>
+
+        <S.Input>
+          <span>Hora</span>
+          <input type="time" placeholder = "Título da tarefa..."></input>
+          <img src = {iconClock} alt = "Relógio"/>
+        </S.Input>
+      
       </S.Form>
 
         <Footer/>
--- src/views/Task/styles.js
+++ src/views/Task/styles.js
@@ -16,6 +16,14 @@ export const Container = styled.div`
         display: flex;
         justify-content: center;
 
+        .inative{
+            opacity: 0.5;
+        }
+
+        button {
+            border: none;
+            background: none;
+        }
         img {
             width: 50px;
             height: 50px;
@@ -25,5 +33,49 @@ export const Container = styled.div`
             &:hover{
                 opacity: 0.5;
             }
+        }
+    `
+
+    export const Input = styled.div`
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        margin: 20px 0;
+
+        span{
+            color: #707070;
+            margin: 5px 0;
+        }
+
+        input {
+            font-size: 16px;
+            padding: 15px;
+            border: none;
+            border-bottom: 1px solid #EE6B26;
+        }
+
+        img {
+            width: 20px;
+            height: 20px;
+            position: relative;
+            left: 90%;
+            bottom: 35px;
+        }
+    `
+
+    export const TextArea = styled.div`
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        margin: 20px 0;
+
+        span{
+            color: #707070;
+            margin: 5px 0;
+        }
+
+        textarea{
+            font-size: 16px;
+            border: 1px solid #EE6B26;
         }
     `
