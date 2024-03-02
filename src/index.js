c91d2ea38acefa8e61f5884407e910d2ba1460a3
Julia dos Santos <santos.ju1995@gmail.com>
2024-03-01 23:16:58 -0300

    navegação tela

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './views/Home';
-import Task from './views/Task';
+
+import Rotas from './rotas';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Task />
+    <Rotas/>
   </React.StrictMode>
 );
